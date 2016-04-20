321944985a14d343476dfff995edd87e2fa3a892
Andrew Selbie <aselbie@gmail.com>
2016-04-20 10:45:50 -0700

    Connector needs domain in webpack config

--- webpack.config.connector.js
+++ webpack.config.connector.js
@@ -1,34 +1,34 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-if (process.env.TRAVIS_BRANCH == 'master') process.env.ENV = 'PROD'
-if (process.env.TRAVIS_BRANCH == 'dev') process.env.ENV = 'DEV'
-if (process.env.TRAVIS_BRANCH == 'qa') process.env.ENV = 'QA'
+if (process.env.TRAVIS_BRANCH === 'master') process.env.ENV = 'PROD'
+if (process.env.TRAVIS_BRANCH === 'dev') process.env.ENV = 'DEV'
+if (process.env.TRAVIS_BRANCH === 'qa') process.env.ENV = 'QA'
 
 
-if (process.env.ENV == 'DEV') {
-  process.env.API_URL = 'https://api.topcoder-dev.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder-dev.com/connector.html';
+if (process.env.ENV === 'DEV') {
+  process.env.DOMAIN = 'topcoder-dev.com'
+  process.env.CONNECTOR_URL = 'https://accounts.topcoder-dev.com/connector.html'
 }
-else if (process.env.ENV == 'QA') {
-  process.env.API_URL = 'https://api.topcoder-qa.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder-qa.com/connector.html';
+else if (process.env.ENV === 'QA') {
+  process.env.DOMAIN = 'topcoder-qa.com'
+  process.env.CONNECTOR_URL = 'https://accounts.topcoder-qa.com/connector.html'
 }
-else if (process.env.ENV == 'PROD') {
-  process.env.API_URL = 'https://api.topcoder.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder-qa.com/connector.html';
+else if (process.env.ENV === 'PROD') {
+  process.env.DOMAIN = 'topcoder.com'
+  process.env.CONNECTOR_URL = 'https://accounts.topcoder-qa.com/connector.html'
 }
 else {
    // for local dev
-  process.env.API_URL = 'https://api.topcoder-dev.com'
-  process.env.CONNECTOR_URL = 'http://local.accounts.topcoder-dev.com:8000/connector.html';
+  process.env.DOMAIN = 'topcoder-dev.com'
+  process.env.CONNECTOR_URL = 'http://local.accounts.topcoder-dev.com:8000/connector.html'
 }
 
-require('coffee-script/register');
+require('coffee-script/register')
 
 const config = require('appirio-tech-webpack-config')({
   dirname: __dirname
-});
+})
 
 module.exports = Object.assign(config, {
   entry: path.join(__dirname, '/connector/connector-embed.js'),
