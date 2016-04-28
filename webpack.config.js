a5f3c699b3b6a7206fab40e1826debaa22919b34
Andrew Selbie <aselbie@gmail.com>
2016-04-28 15:24:50 -0700

    Cleanup unused constants

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,16 @@ if (process.env.TRAVIS_BRANCH === 'qa') process.env.ENV = 'QA'
 
 
 if (process.env.ENV === 'DEV') {
-  process.env.DOMAIN = 'topcoder-dev.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder-dev.com/connector.html'
   process.env.ZENDESK_DOMAIN = 'topcoder.zendesk.com'
 }
 else if (process.env.ENV === 'QA') {
-  process.env.DOMAIN = 'topcoder-qa.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder-qa.com/connector.html'
   process.env.ZENDESK_DOMAIN = 'topcoder.zendesk.com'
 }
 else if (process.env.ENV === 'PROD') {
-  process.env.DOMAIN = 'topcoder.com'
-  process.env.CONNECTOR_URL = 'https://accounts.topcoder.com/connector.html'
   process.env.ZENDESK_DOMAIN = 'topcoder.zendesk.com'
 }
 else {
    // for local dev
-  process.env.DOMAIN = 'topcoder-dev.com'
-  process.env.CONNECTOR_URL = 'http://local.accounts.topcoder-dev.com:8000/connector.html'
   process.env.ZENDESK_DOMAIN = 'kohata.zendesk.com'
 }
 
