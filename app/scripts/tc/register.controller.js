c997dd62a13e5f574e48ea6eb0a3d7765995868d
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-05-04 12:42:48 +0530

    fix

--- app/scripts/tc/register.controller.js
+++ app/scripts/tc/register.controller.js
@@ -26,6 +26,7 @@ import { npad } from '../../../core/utils.js'
     // Set default for toggle password directive
     vm.defaultPlaceholder = 'Create Password'
     vm.busyMessage = BUSY_PROGRESS_MESSAGE
+    vm.retUrl = $stateParams && $stateParams.retUrl ? $stateParams.retUrl : null
 
     vm.countries = ISO3166.getAllCountryObjects()
 
@@ -73,7 +74,7 @@ import { npad } from '../../../core/utils.js'
           }
         }
       }
-      var redirectURL = $stateParams.retUrl ? $stateParams.retUrl : SKILL_PICKER_URL;
+      var redirectURL = vm.retUrl ? vm.retUrl : SKILL_PICKER_URL;
       var body = {
         param: userInfo,
         options: {
