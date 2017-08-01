e544300a22ef2d5197c6a2d910885caf6836e58a
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2017-08-01 15:13:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,8 @@ deploy_s3bucket() {
 		exit 1
 	fi
 	#result=`aws s3 sync ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --cache-control private,no-store,no-cache,must-revalidate,max-age=0`
-	aws s3 sync --dryrun ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --cache-control private,no-store,no-cache,must-revalidate,max-age=0 --include "*.txt" --include "*.js" --include "*.map" --include "*.html" --exclude "*"  --content-encoding gzip
-	result=`aws s3 sync ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --cache-control private,no-store,no-cache,must-revalidate,max-age=0 --include "*.txt" --include "*.js" --include "*.map" --include "*.html" --exclude "*"  --content-encoding gzip`	
+	aws s3 sync --dryrun ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --cache-control private,no-store,no-cache,must-revalidate,max-age=0 --exclude "*" --include "*.txt" --include "*.js" --include "*.map" --include "*.html" --content-encoding gzip
+	result=`aws s3 sync ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --cache-control private,no-store,no-cache,must-revalidate,max-age=0  --exclude "*" --include "*.txt" --include "*.js" --include "*.map" --include "*.html" --content-encoding gzip`	
 	if [ $? -eq 0 ]; then
 		#echo $result
 		echo "All text files are Deployed!"
