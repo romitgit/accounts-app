6befe985599fd49c98631915d50975bed3b1c728
Cristofer Gonzales <crigot@hotmail.com>
2017-11-03 16:33:03 -0300

    Adds configuration override for the embeded connector frame

--- webpack.config.connector.js
+++ webpack.config.connector.js
@@ -14,6 +14,21 @@ const baseConfig = require('appirio-tech-webpack-config')({
   dirname: __dirname
 })
 
+const auth0DevConstants = {
+  auth0Domain : 'topcoder-newauth.auth0.com',
+  AUTH0_DOMAIN : 'topcoder-newauth.auth0.com',
+  ACCOUNTS_APP_URL : 'https://accounts-auth0.topcoder-dev.com/#!/member',
+  ACCOUNTS_APP_CONNECTOR_URL : 'https://accounts-auth0.topcoder-dev.com/connector.html',
+  AUTH0_CLIENT_ID : 'G76ar2SI4tXz0jAyEbVGM7jFxheRnkqc'
+}
+
+Object.assign(process.env, auth0DevConstants)
+
+baseConfig.plugins.forEach(p =>  {
+  if (p.definitions && p.definitions['process.env']) {
+    p.definitions['process.env'] = JSON.stringify(Object.assign(JSON.parse(p.definitions['process.env']), auth0DevConstants))
+  }})
+
 const plugins = baseConfig.plugins.filter( (plugin) => !(plugin instanceof HtmlWebpackPlugin) && !(plugin instanceof CompressionPlugin) )
 
 plugins.push(new HtmlWebpackPlugin({
