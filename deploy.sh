d293c0f5d4f0416224e2cd8f064aaa393e2f72a5
Cristofer Gonzales <crigot@hotmail.com>
2017-11-13 18:21:09 -0300

    Adds deployment of Auth0 Hosted Login Page

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,10 @@ AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 AWS_S3_BUCKET=$(eval "echo \$${ENV}_S3_BUCKET")
 
+AUTH0_DOMAIN=$(eval "echo \$${ENV}_AUTH0_DOMAIN")
+AUTH0_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_DEPLOYMENT_CLIENT_ID")
+AUTH0_CLIENT_SECRET=$(eval "echo \$${ENV}_AUTH0_DEPLOYMENT_CLIENT_SECRET")
+
 configure_aws_cli() {
 	aws --version
 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
@@ -55,6 +59,27 @@ deploy_s3bucket() {
 
 }
 
+deploy_auth0_page() {
+	if [ "$AUTH0_DOMAIN" = "" ] || [ "$AUTH0_CLIENT_ID" = "" ]; then
+		echo "Auth0 deployment configuration missing. Ignoring hosted login page deployment"
+		return
+	fi
+
+	echo "Starting deployment of Auth0 Hosted Login Page"
+
+	echo "{\"pages\":{\"login\":{\"htmlFile\":\"$(sed 's/\"/\\"/g' dist/auth0-hlp.html | tr -d '\n')\",\"metadata\":false,\"name\":\"login\"}}}" > page_deploy.json
+	echo "{\"AUTH0_CLIENT_ID\":\"$AUTH0_CLIENT_ID\", \"AUTH0_DOMAIN\": \"$AUTH0_DOMAIN\"}" > config.json
+	./node_modules/auth0-deploy-cli/index.js -i ./page_deploy.json -c ./config.json -x $AUTH0_CLIENT_SECRET
+	if [ $? -eq 0 ]; then
+		echo "Auth0 Hosted Page deployed successfully"
+	else
+		echo "Auth0 Page Deployment Failed  - $result"
+		# TODO: When configured should this make the build fail although the scripts have been deployed to accounts?
+	fi
+	rm config.json page_deploy.json
+	rm -rd ./local
+}
+
 echo -e "application/font-woff\t\t\t\twoff2" >> /etc/mime.types
 echo -e "application/font-sfnt\t\t\t\tttf" >> /etc/mime.types
 echo -e "application/json\t\t\t\tmap" >> /etc/mime.types
@@ -66,3 +91,4 @@ cat /etc/mime.types  | grep -i ttf
 
 configure_aws_cli
 deploy_s3bucket
+deploy_auth0_page
