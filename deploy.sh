019cfec73e8cfc9a4da4013a5ea5de7974b3ef71
Unknown <mtwomey@beakstar.com>
2018-02-13 22:26:52 -0600

    Pre-merge work 8

--- deploy.sh
+++ deploy.sh
@@ -47,7 +47,7 @@ deploy_s3bucket() {
 	else
 		echo "Deployment Failed  - $result"
 		exit 1
-	fi	
+	fi
 
 }
 
@@ -61,6 +61,7 @@ deploy_auth0_page() {
 
 	echo "{\"pages\":{\"login\":{\"htmlFile\":\"$(sed 's/\"/\\"/g' dist/auth0-hlp.html | tr -d '\n')\",\"metadata\":false,\"name\":\"login\"}}}" > page_deploy.json
 	echo "{\"AUTH0_CLIENT_ID\":\"$AUTH0_DEPLOY_CLIENT_ID\", \"AUTH0_DOMAIN\": \"$AUTH0_DOMAIN\",\"AUTH0_EXCLUDED_RULES\":[\"Global variables and functions\",\"Add custom attributes to access token\"]}" > config.json
+	unset AUTH0_CLIENT_ID
 	./node_modules/auth0-deploy-cli/index.js -i ./page_deploy.json -c ./config.json -x $AUTH0_DEPLOY_CLIENT_SECRET
 	if [ $? -eq 0 ]; then
 		echo "Auth0 Hosted Page deployed successfully"
