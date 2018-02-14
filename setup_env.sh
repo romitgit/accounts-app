9f02d39455e948ad96048c6414b4433deaf55c02
Unknown <mtwomey@beakstar.com>
2018-02-13 21:57:49 -0600

    Pre-merge work 7

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,13 @@
-PREFIX=$1
+if [ "$CIRCLE_BRANCH" == "master" ]; then export ENV=PROD; fi
+if [ "$CIRCLE_BRANCH" == "dev" ]; then export ENV=DEV; fi
+if [ "$CIRCLE_BRANCH" == "qa" ]; then export ENV=QA; fi
+if [ "$CIRCLE_BRANCH" == "dev-auth0" ]; then export ENV=NEWAUTH; fi
 
-export AWS_ACCESS_KEY_ID=$(eval "echo \$${PREFIX}_AWS_ACCESS_KEY_ID")
-export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${PREFIX}_AWS_SECRET_ACCESS_KEY")
-export AWS_S3_BUCKET=$(eval "echo \$${PREFIX}_S3_BUCKET")
-export AUTH0_DOMAIN=$(eval "echo \$${PREFIX}_AUTH0_DOMAIN")
-export AUTH0_CLIENT_ID=$(eval "echo \$${PREFIX}_AUTH0_CLIENT_ID")
-export AUTH0_DEPLOY_CLIENT_ID=$(eval "echo \$${PREFIX}_AUTH0_DEPLOY_CLIENT_ID")
-export AUTH0_DEPLOY_CLIENT_SECRET=$(eval "echo \$${PREFIX}_AUTH0_DEPLOY_CLIENT_SECRET")
-export ACCOUNTS_DOMAIN=$(eval "echo \$${PREFIX}_ACCOUNTS_DOMAIN")
+echo export AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID") >> ~/.circlerc
+echo export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY") >> ~/.circlerc
+echo export AWS_S3_BUCKET=$(eval "echo \$${ENV}_S3_BUCKET") >> ~/.circlerc
+echo export AUTH0_DOMAIN=$(eval "echo \$${ENV}_AUTH0_DOMAIN") >> ~/.circlerc
+echo export AUTH0_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_CLIENT_ID") >> ~/.circlerc
+echo export AUTH0_DEPLOY_CLIENT_ID=$(eval "echo \$${ENV}_AUTH0_DEPLOY_CLIENT_ID") >> ~/.circlerc
+echo export AUTH0_DEPLOY_CLIENT_SECRET=$(eval "echo \$${ENV}_AUTH0_DEPLOY_CLIENT_SECRET") >> ~/.circlerc
+echo export ACCOUNTS_DOMAIN=$(eval "echo \$${ENV}_ACCOUNTS_DOMAIN") >> ~/.circlerc
