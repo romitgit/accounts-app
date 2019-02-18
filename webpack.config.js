bef7ae8219d2292cdf86ce149bccb8c9d89d1850
Vikas Agarwal <vikas.agarwal@topcoder.com>
2019-02-18 14:48:51 +0530

    Github issue#211, Remove dependency of SFDC lead call from Member Trait call — Removed dependency between SFDC lead and member trait call as since Dec 10, 2018, member trait api call is giving 403 for new users who are not active yet. — Hacky way to fix the file stack ES6 code

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 require('./node_modules/coffee-script/register')
 const filter = require('lodash/filter')
+const find = require('lodash/find')
 
 if (process.env.CIRCLE_BRANCH === 'master') process.env.ENV = 'PROD'
 if (process.env.CIRCLE_BRANCH === 'dev') process.env.ENV = 'DEV'
@@ -28,6 +29,16 @@ const config = require('appirio-tech-webpack-config')({
   template: './app/index.jade',
   favicon: './app/images/favicon.ico'
 })
+
+const es6Loader = find(config.module.loaders, loader => loader.test.toString().indexOf(".(js|jsx)" != -1))
+
+
+if (es6Loader) {
+  es6Loader.exclude = /node_modules\/(?!appirio|topcoder|tc|input\-moment|filestack-js)/
+  // if you are seeing this console log after 6 months of its writing, feel free to remove it. it was intended to serve
+  // for debugging in circle ci for some time after the hacky fix for filestack ES6 to ES5 transpilation
+  console.log(es6Loader)
+}
 
 console.log(config.plugins)
 
